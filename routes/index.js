339fc326a0e60cb5b3768556ca9668e8f32e56af
Mallory Payne <mallory.payne18@gmail.com>
2016-04-28 13:12:43 -0400

    out user tweets are populating

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,21 @@ var path = require('path');
 // could use one line instead: var router = require('express').Router();
 var tweetBank = require('../tweetBank');
 
+
+router.get( '/users/:name', function (req, res) {
+	console.log("I'm running");
+  var userName = req.params.name;
+ // var tweets = tweetBank.list();
+ console.log(userName);
+  var userTweet = tweetBank.find({name: userName});
+  console.log(userTweet);
+  res.render( 'index', { title: 'Twitter.js', tweets: userTweet} );
+});
+
 router.get('/', function (req, res) {
   var tweets = tweetBank.list();
   console.log("Tweets here!");
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+  res.render( 'index', { title: 'Twitter.js', tweets: tweets} );
 });
 
 module.exports = router;
--- tweetBank.js
+++ tweetBank.js
@@ -15,23 +15,31 @@ function find (properties) {
 
 module.exports = { add: add, list: list, find: find };
 
-var randArrayEl = function(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-};
+// var randArrayEl = function(arr) {
+//   return arr[Math.floor(Math.random() * arr.length)];
+// };
 
-var getFakeName = function() {
-  var fakeFirsts = ['Nimit', 'Dave', 'Shanna', 'Charlotte', 'Scott', 'Ayana', 'Omri', 'Gabriel', 'Joe'];
-  var fakeLasts = ['Hashington', 'Stackson', 'McQueue', 'OLogn', 'Ternary', 'Claujure', 'Dunderproto', 'Binder', 'Docsreader', 'Ecma'];
-  return randArrayEl(fakeFirsts) + " " + randArrayEl(fakeLasts);
-};
+// var getFakeName = function() {
+//   var fakeFirsts = ['Nimit', 'Dave', 'Shanna', 'Charlotte', 'Scott', 'Ayana', 'Omri', 'Gabriel', 'Joe'];
+//   var fakeLasts = ['Hashington', 'Stackson', 'McQueue', 'OLogn', 'Ternary', 'Claujure', 'Dunderproto', 'Binder', 'Docsreader', 'Ecma'];
+//   return randArrayEl(fakeFirsts) + " " + randArrayEl(fakeLasts);
+// };
 
-var getFakeTweet = function() {
-  var awesome_adj = ['awesome', 'breathtaking', 'amazing', 'funny', 'sweet', 'cool', 'wonderful', 'mindblowing'];
-  return "Fullstack Academy is " + randArrayEl(awesome_adj) + "! The instructors are just so " + randArrayEl(awesome_adj) + ". #fullstacklove #codedreams";
-};
+// var getFakeTweet = function() {
+//   var awesome_adj = ['awesome', 'breathtaking', 'amazing', 'funny', 'sweet', 'cool', 'wonderful', 'mindblowing'];
+//   return "Fullstack Academy is " + randArrayEl(awesome_adj) + "! The instructors are just so " + randArrayEl(awesome_adj) + ". #fullstacklove #codedreams";
+// };
 
-for (var i = 0; i < 10; i++) {
-  module.exports.add( getFakeName(), getFakeTweet() );
-}
+// for (var i = 0; i < 10; i++) {
+//   module.exports.add( getFakeName(), getFakeTweet() );
+// }
+
+module.exports.add("Antha","I am sleepy")
+module.exports.add("Antha","I like Hamilton")
+module.exports.add("Antha","I want Chipotle")
+module.exports.add("Mallory","I like cats!")
+module.exports.add("Mallory","It is lunchtime!")
+module.exports.add("Mallory","Yay New York!")
+
 
 console.log(data);
